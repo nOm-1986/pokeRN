f0f4767ca9cf8b76accd12c45c4b90de1c486bcc
Fabian Beltran <joshefbeltran@gmail.com>
2023-08-18 16:40:43 -0500

    Instalado formik y yup

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, StyleSheet, TextInput, Button, Keyboard } from 'react-native'
 
 export default function LoginForm() {
   return (
-    <View>
-      <Text>LoginForm...</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Iniciar sesión</Text>
+
+        <TextInput 
+          placeholder='Nombre de usuario'
+          style={styles.input}
+          autoCapitalize='none'
+        />
+        <TextInput
+          placeholder='Contraseña' 
+          style={styles.input}
+          autoCapitalize='none'
+          secureTextEntry={true}
+        />
+        <Button title='Entrar' onPress={() => console.log('Entrando...')} />
+      </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: "100%",
+    width: '100%'
+  },
+  
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 15
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    width: '70%'
+  }
+});
